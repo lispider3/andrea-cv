d75390aa98eca799420f80043ecb7b33692942e7
Andrea Lispiteri <andrealispider@gmail.com>
2026-03-03 14:34:03 +0100

    Add title label to capitals quiz during gameplay

--- capitals/capitals.js
+++ capitals/capitals.js
@@ -316,6 +316,7 @@ const render = () => {
     app.innerHTML = `
       <section class="cq-section cq-section--playing">
         <div class="cq-container cq-container--wide">
+          <div class="fb-card-label" style="margin-bottom:12px;padding:0 0 12px;border-bottom:1px solid rgba(255,255,255,0.06)"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17.8 19.2 16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z"/></svg> WORLD CAPITALS QUIZ</div>
           <div class="cq-toolbar">
             <div class="cq-toolbar-left">
               <input type="text" id="cq-input" class="cq-input" placeholder="Type a capital city..." autocomplete="off" autocorrect="off" autocapitalize="none" spellcheck="false" inputmode="text" />
